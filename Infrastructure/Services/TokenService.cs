9279bcaacb11cdf9ad7e8443fa054af13a5dc60a
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-20 17:40:12 +0300

    Extend auth expires time to 3d

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -32,7 +32,7 @@ public class TokenService : ITokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.Now.AddHours(3),
+            Expires = DateTime.Now.AddDays(3),
             SigningCredentials = creds,
         };
 
